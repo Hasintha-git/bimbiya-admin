c1dd044790fb0a3b9eba31b736c652014f5b638c
hasintha_s <hasintha_s@epiclanka.net>
2023-12-31 20:51:38 +0530

    secure false set

--- src/app/utility/constants/end-point.ts
+++ src/app/utility/constants/end-point.ts
@@ -2,7 +2,7 @@ const HOST: string = '104.152.222.98';
 const PORT: string = '8066';
 export const REGISTER: string = 'http://localhost:4200/register';
 
-export const SECURE = true;
+export const SECURE = false;
 
 export const getEndpoint = (isHttps:any) => {
   return `${isHttps ? 'https' : 'http'}://${HOST}:${PORT}/admin`;
